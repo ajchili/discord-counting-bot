7fba9a6fb5744a9fd20c5b619b17d0869a616465
Kirin Patel <kirinpatel@gmail.com>
2024-04-29 23:38:00 -0400

    🤩 get registrator package working

---
+++ packages/registrator/README.md
@@ -0,0 +1,24 @@
+# @discord-counting-bot/registrator
+
+Package used to register commands for the Discord Counting Bot.
+
+## Getting Started
+
+Follow the steps in the root level README.md!
+
+## Usage
+
+1. Create a `config.json` file in the `packages/registrator/src` directory
+   - ```json
+     {
+       "TOKEN": "YOUR_DISCORD_BOT_TOKEN",
+       "CLIENT_ID": "YOUR_DISCORD_BOT_CLIENT_ID"
+     }
+     ```
+1. `npm i`
+1. `npm run build`
+1. `npm run start -w packages/registrator`
+
+## What is command registering?
+
+https://discordjs.guide/creating-your-bot/command-deployment.html#command-registration
--- packages/registrator/src/index.ts
+++ packages/registrator/src/index.ts
@@ -1,3 +1,26 @@
 import { jsonCommands } from "@discord-counting-bot/core";
+import { REST, Routes } from "discord.js";
+import { createRequire } from "node:module";
+import { resolve } from "node:path";
 
-console.log(jsonCommands);
+const require = createRequire(import.meta.url);
+const { TOKEN, CLIENT_ID } = require(
+  resolve(import.meta.dirname, "./config.json")
+);
+
+const rest = new REST({}).setToken(TOKEN);
+
+try {
+  console.log("Started refreshing application (/) commands.");
+
+  const body = Object.values(jsonCommands).reduce(
+    (commands, command) => commands.concat(command.builder.toJSON()),
+    new Array()
+  );
+
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body });
+
+  console.log("Successfully reloaded application (/) commands.");
+} catch (error) {
+  console.error(error);
+}
